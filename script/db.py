7bdd9cc0c4f32a0fcb8a7f0e27791ddea9584788
Ann Keenan <akeenan2@nd.edu>
2017-11-07 21:27:59 -0500

    Baseline calculation

---
+++ script/db.py
@@ -0,0 +1,24 @@
+# db.py
+import pymysql.cursors
+
+class Database_Connection(object):
+    def __init__(self):
+        # Connect to the database
+        self.connection = pymysql.connect(
+                host='localhost',
+                user='yelpuser',
+                password='password',
+                db='yelp_db',
+                charset='utf8mb4',
+                cursorclass=pymysql.cursors.DictCursor)
+
+    def __del__(self):
+        self.connection.close()
+
+    def query(self, sql, args=None):
+        with self.connection.cursor() as cursor:
+            if args:
+                cursor.execute(sql,args)
+            else:
+                cursor.execute(sql)
+        return cursor.fetchall()
